1870ae78d591e572e09ae5ff834865f4a92a2caf
rkakbk83 <87198087+rkakbk83@users.noreply.github.com>
2022-06-30 19:27:30 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+$.ajax({
+    type: 'get',
+    url: 'https://api.covid19api.com/summary',
+    success: function (response) {
+        console.log(response.Countries)
+        for (var i = 0; i < response.Countries.length; i++) {
+            var TotalActive = response.Countries[i].TotalConfirmed - response.Countries[i].TotalRecovered - response.Countries[i].TotalDeaths
+            var tablerow = `<tr><td>${response.Countries[i].Country}</td><td>${response.Countries[i].TotalConfirmed}</td><td>${TotalActive}</td><td>${response.Countries[i].TotalRecovered}</td><td>${response.Countries[i].TotalDeaths}</td></tr>`
+
+            $('#tbody').append(tablerow)
+        }
+        $('#covidtable').DataTable()
+    },
+    error: function (error) {
+        console.log(error)
+    }
+})
